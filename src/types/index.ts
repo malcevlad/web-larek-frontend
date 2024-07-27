897d343c16c5f0fcf3adc75cd92be7f4c316bde2
Vladislav Maltsev <malcelvad777@yandex.ru>
2024-07-27 14:24:44 +0300

    8-th project 1-st review

---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+// Интерфейс данных карточки приходящей с сервера
+export interface ICard {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+
+// Интерфейс для форм
+export interface IForms {
+    payment: string;
+    address: string;
+    email: string;
+    phone: string;
+}
+
+// Интерфейс для модели данных приложения
+export interface IAppState {
+    catalog: ICard[];
+    preview: string | null;
+    order: IOrder | null;
+    basket: IBasketItem[];
+}
+
+// Интерфейс данных о заказе отправляемых на сервер
+export interface IOrder extends IForms {
+    items: string[];
+    total: number;
+}
+
+// Тип ответа от сервера при оформлении заказа
+export interface IOrderResult {
+    id: string;
+    total: number;
+}
+
+// Тип, с потенциальными ошибками для полей формы заказа
+export type IFormErrors = Partial<Record<keyof IForms, string>>;
+
+// Тип товара в корзине
+export type IBasketItem = Pick<ICard, 'id' | 'title' | 'price'>;
